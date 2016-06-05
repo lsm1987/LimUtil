472682eb098f0bc533531b7241d608d646fefc83
lsm1987 <1987lsm@gmail.com>
2016-06-05 16:57:17 +0900

    [Xml] OneLineHashSetTest 에 Enum 테스트 추가

--- Assets/Scripts/Xml/Editor/OneLineHashSetTest.cs
+++ Assets/Scripts/Xml/Editor/OneLineHashSetTest.cs
@@ -8,10 +8,17 @@ public class OneLineHashSetTest
     [XmlRoot("root")]
     public class ItemInfo
     {
+        public enum ItemType
+        {
+            Weapon, Equip, Consume,
+        }
+
         [XmlElement("items")]
         public OneLineHashSet<int> Items { get; set; }
         [XmlElement("itemIds")]
         public OneLineHashSet<ItemId> ItemIds { get; set; }
+        [XmlElement("itemTypes")]
+        public OneLineHashSet<ItemType> ItemTypes { get; set; }
     }
 
     [Test]
@@ -20,12 +27,14 @@ public class OneLineHashSetTest
         const string xmlString = @"<root>
   <items>10,20,30</items>
   <itemIds>1000,2000</itemIds>
+  <itemTypes>Equip,Consume</itemTypes>
 </root>";
 
         ItemInfo info = XmlUtil.LoadXmlFromString<ItemInfo>(xmlString);
         Assert.AreEqual(3, info.Items.Count);
         Assert.IsTrue(info.Items.Contains(10));
         Assert.IsTrue(info.ItemIds.Contains(new ItemId(1000)));
+        Assert.IsTrue(info.ItemTypes.Contains(ItemInfo.ItemType.Equip));
     }
 
     [Test]
@@ -42,9 +51,14 @@ public class OneLineHashSetTest
         info.ItemIds.Add(new ItemId(1000));
         info.ItemIds.Add(new ItemId(2000));
 
+        info.ItemTypes = new OneLineHashSet<ItemInfo.ItemType>();
+        info.ItemTypes.Add(ItemInfo.ItemType.Equip);
+        info.ItemTypes.Add(ItemInfo.ItemType.Consume);
+
         const string expectedXmlString = @"<root>
   <items>10,20,30</items>
   <itemIds>1000,2000</itemIds>
+  <itemTypes>Equip,Consume</itemTypes>
 </root>";
 
         string xmlString = XmlUtil.SaveXmlToString(info);
